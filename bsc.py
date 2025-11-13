f63cde3484798d10434b34389f280fcd74b664a8
Anna Sedeleva <anyuta18031991@yandex.ru>
2025-11-14 00:29:33 +0300

    moralis

--- bsc.py
+++ bsc.py
@@ -10,20 +10,12 @@ class BscScanScraper:
     def __init__(self):
         self.session = requests.Session()
         self.session.headers.update({
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'ru-RU,ru;q=0.9,en;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Cache-Control': 'max-age=0'
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 })
-        '''self.session.proxies.update({
+        self.session.proxies.update({
             "http": "http://qWD41r:JdCPrA@196.19.121.132:8000"
-        })'''
+        })
         self.wallets_dict = {}
 
 
