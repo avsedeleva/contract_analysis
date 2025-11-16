4f2950320a17d49eecd40ef60cba6ebf827984ed
Anna Sedeleva <anyuta18031991@yandex.ru>
2025-11-17 00:32:35 +0300

    moralis

--- moralis.py
+++ moralis.py
@@ -15,15 +15,71 @@ class MoralisScraper:
             "X-API-Key": API_MORALIS
         }
         self.holders = []
+        #self.token_list = {}
+        self.top_tokens= {}
+        self.number = 0
+        self.contract = None
 
     def get_wallet_balance(self, wallet_address):
-        url = f"https://deep-index.moralis.io/api/v2.2/{wallet_address}/erc20?chain=eth"
-        response = requests.request("GET", url, headers=self.headers)
-        print(json.dumps(response.json(), indent=4))
-        return response.json()
+        tokens = []
+        #url = f"https://deep-index.moralis.io/api/v2.2/{wallet_address}/erc20?chain=eth"
+        #url1 = f"https://deep-index.moralis.io/api/v2.2/wallets/{wallet_address}/tokens?chain=bsc&limit=25"
+        #url1 = f"https://deep-index.moralis.io/api/v2.2/wallets/{wallet_address}/tokens"
+        #url1 = f""
+        url_dict = {
+            'eth': f"https://deep-index.moralis.io/api/v2.2/wallets/{wallet_address}/tokens?chain=eth&limit=100",
+            'bsc': f"https://deep-index.moralis.io/api/v2.2/wallets/{wallet_address}/tokens?chain=bsc&limit=100",
+            #'base': f"https://deep-index.moralis.io/api/v2.2/wallets/{wallet_address}/tokens?chain=base&limit=100"
+        }
+        for chain, url in url_dict.items():
+            response = requests.request("GET", url, headers=self.headers)
+            try:
+                data = response.json().get('result')
+                #print(data)
+            except:
+                continue
+            for info in data:
+                #print(info)
+                if info['possible_spam'] or info['security_score'] is None:
+                    continue
+                if info['security_score'] < 50:
+                    continue
+                if float(info.get('usd_value')) > 100:
+                    tokens.append({
 
-    def get_top_holders(self, contract):
-        url = f"https://deep-index.moralis.io/api/v2.2/erc20/{contract}/owners?chain=bsc&limit=100&order=DESC"
+                            'chain': chain,
+                            'name': info.get('symbol'),
+                            'address': info.get('token_address'),
+                            'quantity': round(float(info.get('balance_formatted')), 0),
+                            'usd_value': round(float(info.get('usd_value')), 0)
+
+                    }
+                    )
+        sorted_tokens = sorted(tokens, key=lambda x: x['usd_value'], reverse=True)
+        # print(sorted_tokens[0:10])
+        if len(sorted_tokens) >= 2:
+            token_list = sorted_tokens[0:30]
+        #print(token_list)
+        #print(json.dumps(self.token_list, indent=4))
+            for tok in token_list:
+                if tok['address'].lower() != self.contract.lower():
+                    i = f"{tok['chain']}_{tok['address']}_{tok['name']}"
+                    if not self.top_tokens.get(i):
+                        self.top_tokens[i] = {
+                        'chain': tok['chain'],
+                        'address': tok['address'],
+                        'name': tok['name'],
+                        'number': 1
+                        }
+                    else:
+                        self.top_tokens[i]['number'] += 1
+
+            return token_list
+        return
+
+    def get_top_holders(self, contract, input_contract_number):
+        self.contract = contract
+        url = f"https://deep-index.moralis.io/api/v2.2/erc20/{contract}/owners?chain=bsc&limit={input_contract_number}&order=DESC"
         response = requests.request("GET", url, headers=self.headers)
         count = 0
         for info in response.json().get("result"):
@@ -37,11 +93,20 @@ class MoralisScraper:
                     'number': count
                     #'percent': round(float(info.get("21.253917305601945")), 2)
                 })
-        #print(self.holders)
+
         return self.holders
 
+    def get_top_tokens(self):
+        top_tokens_list = []
+        for tok, info in self.top_tokens.items():
 
-wallet_address = '0x4858821da3a75c001a895072305b774af7a494d9'
+            top_tokens_list.append(info)
+        sorted_info = sorted(top_tokens_list, key=lambda x: x['number'], reverse=True)
+        if len(sorted_info) > 30:
+            return sorted_info[0:30]
+        return sorted_info
+
+wallet_address = '0x6f6c1073266902197350463620591c0ff9467e85'#'0x7485e27650058d22590ccdee1a86604c71dd8ad8'#'0xd17974cd64ad371b387baeaef2f27c0e8e67f159' #'0x4858821da3a75c001a895072305b774af7a494d9'
 contract = "0xd98438889Ae7364c7E2A3540547Fad042FB24642"
-MoralisScraper().get_top_holders(contract=contract)
+#MoralisScraper().get_wallet_balance(wallet_address)
 #get_wallet_balance(wallet_address)
--- tg_bot.py
+++ tg_bot.py
@@ -27,15 +27,31 @@ async def cmd_start(message: types.Message, state: FSMContext):
 # --- Опросная часть ---
 @dp.message(Data.waiting_for_contract)
 async def process_contract(message: types.Message, state: FSMContext):
+    input_number_contract =100
     contract = message.text
     if '0x' not in contract:
         await message.answer("Контракт не корректный, введи еще раз")
-    tokens_dict = await analyze(contract)
+    tokens_dict = await analyze(contract, input_number_contract)
+    top_tokens = mrl_scraper.get_top_tokens()
+    msg_general = (f'{len(tokens_dict)}/{input_number_contract} contracts\n'
+                   f'TOP 20 TOKENS\n')
+    for tok in top_tokens:
+        if tok["chain"] == 'bsc':
+            msg_general += f'{tok["chain"]} <a href="https://bscscan.com/address/{tok["address"]}"><b>{tok["name"]}</b></a>: {tok["number"]}\n'
+        else:
+            msg_general += f'{tok["chain"]} <a href="https://etherscan.io/address/{tok["address"]}"><b>{tok["name"]}</b></a>: {tok["number"]}\n'
+    await message.answer(msg_general, disable_web_page_preview=True)
     for address, tok_list in tokens_dict.items():
-        msg = f"<code>{address}</code>\n<i>Chain    Name    USD value</i>\n"
-        for tok in tok_list:
-            msg += f"{tok['chain']} <b>{tok['name']}</b> {tok['usd_value']:,.2f}\n"
-        await message.answer(msg)
+        tokens = tok_list['tokens']
+        number = tok_list['number']
+        usd_value = f"{tok_list['usd_value']:,.2f}"
+        msg = f'{number}  <a href="https://bscscan.com/address/{address}#asset-tokens">BSC {address}</a>\n<a href="https://etherscan.io/address/{address}#asset-tokens">ETH {address}</a>\n<i>USD VALUE:</i> {usd_value}\n<i>Chain    Name    USD value</i>\n'
+        for tok in tokens:
+            if tok["chain"] == 'bsc':
+                msg += f'{tok["chain"]} <a href="https://bscscan.com/address/{tok["address"]}"><b>{tok["name"]}</b></a> {tok["usd_value"]:,.2f}\n'
+            else:
+                msg += f'{tok["chain"]} <a href="https://etherscan.io/address/{tok["address"]}"><b>{tok["name"]}</b></a> {tok["usd_value"]:,.2f}\n'
+        await message.answer(msg, disable_web_page_preview=True)
 
 
 # --- Utilits ---
@@ -43,13 +59,18 @@ async def select_option(message: types.Message, state: FSMContext):
     await message.answer('Введи контракт')
     await state.set_state(Data.waiting_for_contract)
 
-async def analyze(contract):
-    holders_list = mrl_scraper.get_top_holders(contract)
+async def analyze(contract, input_contract_number):
+    holders_list = mrl_scraper.get_top_holders(contract, input_contract_number)
     tokens_dict = {}
     for holder_info in holders_list:
-        tokens = bsc_scraper.get_wallet_balance(holder_info.get('address'))
-        tokens_dict[holder_info.get('address')] = tokens
-    print(json.dumps(tokens_dict, indent=4))
+        tokens = mrl_scraper.get_wallet_balance(holder_info.get('address'))
+        if tokens:
+            tokens_dict[holder_info.get('address')] = {}
+            tokens_dict[holder_info.get('address')]['tokens'] = tokens
+            tokens_dict[holder_info.get('address')]['number'] = holder_info.get('number')
+            tokens_dict[holder_info.get('address')]['usd_value'] = holder_info.get('usd_value')
+            tokens_dict[holder_info.get('address')]['stat_number'] = len(tokens)
+    #print(json.dumps(tokens_dict, indent=4))
     return tokens_dict
 
 # Запуск бота
